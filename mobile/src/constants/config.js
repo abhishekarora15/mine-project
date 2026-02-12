6b8659a79001c913278ceecc66c2e0f287939b52
abhishekarora15 <aroraabhishek4807@gmail.com>
2026-02-12 19:00:10 +0530

    Update mobile API_URL to production Render URL

--- mobile/src/constants/config.js
+++ mobile/src/constants/config.js
@@ -1,4 +1,2 @@
-import { Platform } from 'react-native';
-
 // Use your computer's local IP address if testing on a physical device
-export const API_URL = 'http://192.168.29.50:5000/api';
+export const API_URL = 'https://mine-project-fz5s.onrender.com/api';
