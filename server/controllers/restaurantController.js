116f84e530caeb64d88dc1e4d1ee179709b56ced
abhishekarora15 <aroraabhishek4807@gmail.com>
2026-02-15 11:44:44 +0530

    feat: implement geospatial restaurant search with production-grade patterns

--- server/controllers/restaurantController.js
+++ server/controllers/restaurantController.js
@@ -1,82 +1,68 @@
 const Restaurant = require('../models/Restaurant');
-const { success, error } = require('../utils/responseFormatter');
+const { success } = require('../utils/responseFormatter');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
-exports.createRestaurant = async (req, res) => {
-    try {
-        const { name, description, cuisineTypes, location, isCloudKitchen, image } = req.body;
+exports.createRestaurant = catchAsync(async (req, res, next) => {
+    const { name, description, cuisineTypes, location, isCloudKitchen, image } = req.body;
 
-        const newRestaurant = await Restaurant.create({
-            ownerId: req.user._id,
-            name,
-            description,
-            cuisineTypes,
-            location,
-            isCloudKitchen,
-            image
-        });
+    const newRestaurant = await Restaurant.create({
+        ownerId: req.user._id,
+        name,
+        description,
+        cuisineTypes,
+        location,
+        isCloudKitchen,
+        image
+    });
 
-        success(res, { restaurant: newRestaurant }, 201);
-    } catch (err) {
-        error(res, err.message, 400);
-    }
-};
+    success(res, { restaurant: newRestaurant }, 201);
+});
 
-exports.getAllRestaurants = async (req, res) => {
-    try {
-        const { lat, lng, radius = 5000, cuisine } = req.query; // radius in meters
-        let query = {};
+exports.getAllRestaurants = catchAsync(async (req, res, next) => {
+    const { lat, lng, radius = 5000, cuisine } = req.query; // radius in meters
+    let query = {};
 
-        // 1) Location-based filtering (Geospatial)
-        if (lat && lng) {
-            query.location = {
-                $near: {
-                    $geometry: {
-                        type: 'Point',
-                        coordinates: [parseFloat(lng), parseFloat(lat)],
-                    },
-                    $maxDistance: parseInt(radius),
+    // 1) Location-based filtering (Geospatial)
+    if (lat && lng) {
+        query.location = {
+            $near: {
+                $geometry: {
+                    type: 'Point',
+                    coordinates: [parseFloat(lng), parseFloat(lat)],
                 },
-            };
-        }
-
-        // 2) Cuisine filtering
-        if (cuisine) {
-            query.cuisineTypes = { $in: cuisine.split(',') };
-        }
-
-        const restaurants = await Restaurant.find(query);
-        success(res, { restaurants });
-    } catch (err) {
-        error(res, err.message, 400);
+                $maxDistance: parseInt(radius),
+            },
+        };
     }
-};
 
-exports.getRestaurant = async (req, res) => {
-    try {
-        const restaurant = await Restaurant.findById(req.params.id);
-        if (!restaurant) {
-            return error(res, 'No restaurant found with that ID', 404);
-        }
-        success(res, { restaurant });
-    } catch (err) {
-        error(res, err.message, 400);
+    // 2) Cuisine filtering
+    if (cuisine) {
+        query.cuisineTypes = { $in: cuisine.split(',') };
     }
-};
 
-exports.updateRestaurant = async (req, res) => {
-    try {
-        const restaurant = await Restaurant.findOneAndUpdate(
-            { _id: req.params.id, ownerId: req.user._id },
-            req.body,
-            { new: true, runValidators: true }
-        );
+    const restaurants = await Restaurant.find(query);
+    success(res, { restaurants });
+});
 
-        if (!restaurant) {
-            return error(res, 'No restaurant found or you are not the owner', 404);
-        }
-
-        success(res, { restaurant });
-    } catch (err) {
-        error(res, err.message, 400);
+exports.getRestaurant = catchAsync(async (req, res, next) => {
+    const restaurant = await Restaurant.findById(req.params.id);
+    if (!restaurant) {
+        return next(new AppError('No restaurant found with that ID', 404));
     }
-};
+    success(res, { restaurant });
+});
+
+exports.updateRestaurant = catchAsync(async (req, res, next) => {
+    const restaurant = await Restaurant.findOneAndUpdate(
+        { _id: req.params.id, ownerId: req.user._id },
+        req.body,
+        { new: true, runValidators: true }
+    );
+
+    if (!restaurant) {
+        return next(new AppError('No restaurant found or you are not the owner', 404));
+    }
+
+    success(res, { restaurant });
+});
